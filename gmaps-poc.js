45f0037c40d1676c51fa3eb9f63f449b4ba4a5c3
Mateusz Ciężczak <juicedrink312@gmail.com>
2018-03-22 16:52:24 +0100

    mock data

--- gmaps-poc.js
+++ gmaps-poc.js
@@ -2,13 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mock_data = [];
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomData () {
+  for (i = 0; i < 20; i++) { 
+    mock_data.push({
+      id: i,
+      lat: getRandomArbitrary(-90, 90),
+      lon: getRandomArbitrary(-180, 180),
+      value: getRandomArbitrary(0,10) 
+    });
+}
+}
+
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  io.emit('data', ['hello'])
+  console.log('User connected');
+  mock_data = [];
+  getRandomData();
+  io.emit('data', mock_data);
 });
 
 
